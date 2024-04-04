79799e05e09af2fce822508e64431e9f8431b2dc
baglana <102351104+baglana@users.noreply.github.com>
2024-04-04 19:48:36 +0500

    Add MoreInfo markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Quote from './components/Quote'
 import StatusMessage from "./components/StatusMessage.tsx";
 import MoreLessButton from "./components/MoreLessButton.tsx";
+import MoreInfo from "./components/MoreInfo.tsx";
 
 function App() {
     return (
@@ -10,11 +11,7 @@ function App() {
             <StatusMessage/>
 
             <MoreLessButton/>
-
-            Current timezone
-            Day of the year
-            Day of the week
-            Week number
+            <MoreInfo/>
         </>
     )
 }
---
+++ src/components/MoreInfo.tsx
@@ -0,0 +1,22 @@
+export default function MoreInfo() {
+    return (
+        <footer>
+            <section>
+                <h2>Current timezone</h2>
+                <p>Europe/London</p>
+            </section>
+            <section>
+                <h2>Day of the year</h2>
+                <p>295</p>
+            </section>
+            <section>
+                <h2>Day of the week</h2>
+                <p>5</p>
+            </section>
+            <section>
+                <h2>Week number</h2>
+                <p>42</p>
+            </section>
+        </footer>
+    )
+}
