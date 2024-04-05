7bbe19fd9ad1939e0a3a9033b158a14f5b09cac7
baglana <102351104+baglana@users.noreply.github.com>
2024-04-05 11:43:07 +0500

    Add quote base style

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,15 +1,18 @@
 import RefreshButton from "./RefreshButton.tsx";
+import "../styles/quote.css";
 
 export default function Quote() {
-    return (
-        <article>
-            <blockquote>
-                <p>“The science of operations, as derived from mathematics more especially, is a science of itself, and
-                    has
-                    its own abstract truth and value.”</p>
-                <footer>Ada Lovelace</footer>
-            </blockquote>
-            <RefreshButton/>
-        </article>
-    )
+  return (
+    <article>
+      <blockquote>
+        <p>
+          “The science of operations, as derived from mathematics more
+          especially, is a science of itself, and has its own abstract truth and
+          value.”
+        </p>
+        <footer>Ada Lovelace</footer>
+      </blockquote>
+      <RefreshButton />
+    </article>
+  );
 }
